dd74cc384f64e11d7604d467eca7cbbdbc8ca680
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-08 08:21:45 +0000

    variable Understanding in JS

---
+++ Basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=1234;
+let accountEmail="anteshwarwagh15@gmail.com"
+var accountPassword="Antu@2020"
+accountCity="Jaipur"
+let accountState;
+//accountId=2 Not allowed in js 
+accountEmail="hiteshchoudhary@gmail.com"
+accountPassword="12121212"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
