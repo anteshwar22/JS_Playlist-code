8e0b0b7f3070fdf7044b6f988b707ba97ba6a1e4
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-09 12:15:14 +0000

    update the conversion

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -1,18 +1,18 @@
-// console.log("2">2); //false 
-// console.log(2>3);  //false
-// console.log(2>=2);//True
-// console.log(2<=3);//true
-// console.log(2<3); //true
-// console.log(2==2);//true
-// console.log(2!=2);//false
+console.log("2">2); //false 
+console.log(2>3);  //false
+console.log(2>=2);//True
+console.log(2<=3);//true
+console.log(2<3); //true
+console.log(2==2);//true
+console.log(2!=2);//false
 
-// console.log(null==0);
-// console.log(null>=0);
-// console.log(null<=0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
 
-// console.log(undefined==0)
-// console.log(undefined>=0)
-// console.log(undefined<=0);
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<=0);
 
 
 
