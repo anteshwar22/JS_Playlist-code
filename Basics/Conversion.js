e92d9e1c79ccfbab26267d01e68d5d94857c6ff7
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-09 12:14:12 +0000

    confusion in Conversion

---
+++ Basics/Conversion.js
@@ -0,0 +1,21 @@
+// console.log("2">2); //false 
+// console.log(2>3);  //false
+// console.log(2>=2);//True
+// console.log(2<=3);//true
+// console.log(2<3); //true
+// console.log(2==2);//true
+// console.log(2!=2);//false
+
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined<=0);
+
+
+
+
+
+
