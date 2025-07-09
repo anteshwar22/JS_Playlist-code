df81039832b780419a8608da267f40b524d30ca5
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-09 11:18:42 +0000

    hi Dude done HM

--- Basics/StringConversion.js
+++ Basics/StringConversion.js
@@ -1,4 +1,13 @@
  let num=3
  let Negative=-num
  console.log(Negative);
+ let str1="hello"
+ let str2="Hitesh"
+ let str3=str1+str2
+ console.log(str3) 
+ console.log(2+2);//4
+ console.log("1"+2+2);//122
+ console.log(1+2+"3");//33
+ 
+ 
  
---
+++ Basics/trike.js
@@ -0,0 +1,13 @@
+// // console.log(true)
+// // console.log(true)
+// // console.log(+true)
+// let gamecounter=100
+// ++gamecounter;
+// console.log(gamecounter);
+
+// let x=2;
+// let y=x++;
+let x=3n;
+let y=++x;
+console.table([x,y]);
+ 
