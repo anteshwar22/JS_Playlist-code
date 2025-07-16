b93c693c734ba17794e7b84744610de67608ec6e
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-16 10:40:35 +0000

    Dates in JavaScript

---
+++ Basics/Datesin.js
@@ -0,0 +1,23 @@
+// // Dates in Javascript
+ 
+// let myDate =new Date()
+// // console.log(myDate.toDateString())
+// // console.log(myDate.toLocaleDateString());
+// // console.log(myDate.toLocaleString());
+// // console.log(typeof myDate);
+
+// // let mycreateddate =new Date(2023,0,23)
+// // console.log(mycreateddate.toDateString());
+
+// // let newDate =new Date(2025,7,13,5,4);
+// // console.log(newDate.toLocaleString())
+//  let Birthday =new Date("2024-01-24");
+//  //console.log(Birthday.toLocaleString());
+// let myTimeStamp =Date.now()
+// // console.log(myTimeStamp) 
+// console.log(Math.floor(Date.now()/1000));
+// // output is  :- 1752662034
+// console.log(Date.getday())
+ let newDate =new Date()
+ console.log(newDate);
+ 
