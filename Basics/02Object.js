9800701113c896f3ad1fee9144e4ca2cb9ad3bb4
Anteshwar Waghmare <160599252+anteshwar22@users.noreply.github.com>
2025-07-18 10:48:17 +0000

    Object Destructuring in js

--- Basics/02Object.js
+++ Basics/02Object.js
@@ -22,9 +22,9 @@ const obj1={1:"a",2:"b"}
 const obj2={3:"c",4:"d"}
 const obj4={5:"e",6:"f"}
 //const obj3=Object.assign({},obj1,obj2,obj4)//output { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
-//object.assign(target,source1,source2,...,sourceN)
+//object.assign(target,souq rce1,source2,...,sourceN)
 const Spreadmethod={...obj1,...obj2,...obj4}
-console.log(Spreadmethod);
+//console.log(Spreadmethod);
 //output is:-
 //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 
@@ -42,12 +42,27 @@ const users=[
         email:"antu@gmail.com"
     }
 ] 
-console.log(users[1].email)//antu@gmail.com
-console.log(tinderUser);
+// console.log(users[1].email)//antu@gmail.com
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-// it return value in array format 
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// // it return value in array format 
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('loggedin'));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('loggedin'));
+ 
+const course=
+{
+    coursename:"Js In Hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+const{ courseInstructor:Instructor}=course
+console.log(courseInstructor);
+console.log(Instructor);
+
+// console.log(coursename)
+// console.log(price);
+
+
---
+++ Basics/03ObjectDeStructuring.js
@@ -0,0 +1,16 @@
+const User={
+    name:"Anteshwar Laxman Waghmare",
+    email:"antu@microsoft.com",
+    college:"Sinhgad Institute of Technology Pune",
+    year:3,
+    regular:true
+}
+const{name,email,college,year,regular}=User
+console.log(name);
+console.log(email);
+console.log(college);
+console.log(year);
+console.log(regular);
+
+
+
